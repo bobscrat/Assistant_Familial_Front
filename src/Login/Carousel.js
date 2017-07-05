2055e1ef9a3fdf02a308d2525d7990137f73f932
Didier4487 <Didier4487>
2017-07-05 17:16:49 +0200

    ajout nouvelles images carrousel

--- src/Login/Carousel.js
+++ src/Login/Carousel.js
@@ -10,6 +10,8 @@ import image2 from './images/02.png';
 import image3 from './images/03.png';
 import image4 from './images/04.png';
 import image5 from './images/05.png';
+import image6 from './images/06.png';
+import image7 from './images/07.png';
 
 class CarouselACDO extends Component{
     render(){
@@ -20,19 +22,32 @@ class CarouselACDO extends Component{
                 height='400px'
             >
                 <div>
-                    <Image src={image1} centered/>                    
+                    <Image src={image1} centered/>     
+                    <p className='legend'>Difficile de vous organiser</p>               
                 </div>
                 <div>
                     <Image src={image2} centered/> 
+                    <p className='legend'>OLGA vous assiste pour les RDV</p>
                 </div>
                 <div>
                     <Image src={image3} centered/> 
+                    <p className='legend'>De toute la famille</p>
                 </div>
                 <div>
                     <Image src={image4} centered/> 
+                    <p className='legend'>Pour vos projets également</p>
                 </div>
                 <div>
                     <Image src={image5} centered/> 
+                    <p className='legend'>Les vacances</p>
+                </div>
+                <div>
+                    <Image src={image6} centered/> 
+                    <p className='legend'>L'éducation</p>
+                </div>
+                <div>
+                    <Image src={image7} centered/> 
+                    <p className='legend'>Venez découvrir OLGA</p>
                 </div>
             </Carousel>
         )
